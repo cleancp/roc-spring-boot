d65f0a5ebbed5e7833deaa7f6f551ff9232a4213
zouchangpeng <zouchangpeng@zlsd.com>
2021-03-04 16:57:42 +0800

    cros config

---
+++ spring-boot-tomcat/src/main/java/com/today/roc/springboot/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.today.roc.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zou.cp
+ * @version 1.0
+ * @Description
+ * @createTime 2021年03月03日 10:55*
+ * log.info()
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
+
+
